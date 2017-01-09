0942112fabd38ff6a1c3f0d9d7a6ac0db69a011f
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2017-01-09 15:11:57 +0000

    Create gulp task to reload server on html/css or javascript changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,27 @@
 var gulp = require('gulp');
-
+var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
-    .pipe(sass()) // Using gulp-sass
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass())
     .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
-  // Other watchers
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
