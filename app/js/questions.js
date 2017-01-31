c23257cd0689a162714016bfcf11a2f1d59c21d5
Moomba Muloba <moomba.muloba@cohaesus.co.uk>
2017-01-31 11:03:00 +0000

    Render JSON data to Index.html using JavaScript

--- app/js/questions.js
+++ app/js/questions.js
@@ -1,9 +1,62 @@
-// request JSON data 
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'api.json');
-
-ourRequest.onload = function(){
-  console.log(ourRequest.responseText);
+//fire the javascript when the document has loaded 
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+  	console.log("Dom is loading");
+    document.addEventListener('DOMContentLoaded', weAreReady);
+  }
 }
 
-ourRequest.send();
+ready(weAreReady);
+
+function weAreReady(){
+	console.log("Dom is Ready!");
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'api.json');
+
+	xhr.onload = function(){
+	  var ourData = xhr.responseText;
+	  renderQuestionnaire(ourData); // Run function on the data object after successful JSON call 
+	}
+
+	xhr.send();
+}
+
+//Render the survey interface from scratch  
+function renderQuestionnaire(data){
+	var parsed = JSON.parse(data);
+	var htmlString = "";
+
+	htmlString += '<h5 id="heading">' + parsed[0].heading + '</h5>';
+
+  for (i = 0; i < parsed[1].choices.length ; i++) {
+
+  	htmlString += '<label class="block">'; 
+  	htmlString += '<input type="radio" class="u-margin-right-tiny" name="radgroup" value="B">';
+  	htmlString += parsed[0].choices[i].question;
+    htmlString +=  '</label>'; 
+  }
+
+  form.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
+
+//add event listeners for button elements 
+var next = document.getElementById("btn-next");
+var prev = document.getElementById("btn-prev");
+var head = document.getElementById("heading");
+var question = document.getElementById("question");
+var form = document.getElementById("myForm");
+var title = document.getElementById("h1");
+
+
+//create a function that will show the next qustion on next button click
+
+
+// next.addEventListener("click", getMyData);
+
+
+
+
